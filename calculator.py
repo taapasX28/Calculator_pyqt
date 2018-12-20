b4acd85d0487b7577e078b52d7d5598684801d22
Venkatesh Dwivedi <dwivedivenkatesh98@gmail.com>
2018-12-20 09:46:25 +0530

    improved ui and added complex mode (#26)
    * improved ui and added complex mode
    
    * improved complex mode and ui
    
    * improved complex mode and ui
    
    * modified complex mode
    
    * modified complex mode

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'calculator.ui'
+#
+# Created by: PyQt4 UI code generator 4.12.1
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
@@ -18,7 +25,183 @@ except AttributeError:
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
-        Dialog.resize(367, 478)
+        Dialog.resize(386, 478)
+        Dialog.setStyleSheet(_fromUtf8("\n"
+"QToolTip\n"
+"{\n"
+"     border: 1px solid black;\n"
+"     background-color: #D1DBCB;\n"
+"     padding: 1px;\n"
+"     border-radius: 3px;\n"
+"     opacity: 100;\n"
+"}\n"
+"\n"
+"QWidget\n"
+"{\n"
+"    color: #b1b1b1;\n"
+"    background-color: #323232;\n"
+"    selection-background-color:#323232;\n"
+"    selection-color: black;\n"
+"    background-clip: border;\n"
+"    border-image: none;\n"
+"    border: 0px transparent black;\n"
+"    outline: 0;\n"
+"}\n"
+"\n"
+"QWidget:item:hover\n"
+"{\n"
+"    background-color: #D1DBCB;\n"
+"    color: black;\n"
+"}\n"
+"\n"
+"QWidget:item:selected\n"
+"{\n"
+"    background-color: #D1DBCB;\n"
+"    border: 0px\n"
+"}\n"
+"\n"
+"QLineEdit\n"
+"{\n"
+"    background-color: #1e1e1e;\n"
+"    selection-background-color: #D1DBCB;\n"
+"    selection-color: black;\n"
+"    padding: 5px;\n"
+"    border-style: solid;\n"
+"    border: 1px solid #76797C;\n"
+"    border-radius: 2px;\n"
+"    color: #eff0f1;\n"
+"}\n"
+"QPushButton::menu-indicator  {\n"
+"    subcontrol-origin: padding;\n"
+"    subcontrol-position: bottom right;\n"
+"    left: 8px;\n"
+"}\n"
+"QPushButton\n"
+"{\n"
+"    color: #eff0f1;\n"
+"    background-color: #323232;\n"
+"    border-width: 1px;\n"
+"    border-color: #76797C;\n"
+"    border-style: solid;\n"
+"    padding: 5px;\n"
+"    border-radius: 0px;\n"
+"    outline: none;\n"
+"}\n"
+"\n"
+"QPushButton:disabled\n"
+"{\n"
+"    background-color: #323232;\n"
+"    border-width: 1px;\n"
+"    border-color: #454545;\n"
+"    border-style: solid;\n"
+"    padding-top: 5px;\n"
+"    padding-bottom: 5px;\n"
+"    padding-left: 10px;\n"
+"    padding-right: 10px;\n"
+"    border-radius: 2px;\n"
+"    color: #454545;\n"
+"}\n"
+"\n"
+"QPushButton:focus {\n"
+"    background-color: #D1DBCB;\n"
+"    color: black;\n"
+"}\n"
+"\n"
+"QPushButton:pressed\n"
+"{\n"
+"    color: black;\n"
+"    background-color: #D1DBCB;\n"
+"    padding-top: -15px;\n"
+"    padding-bottom: -17px;\n"
+"}\n"
+"\n"
+"QComboBox\n"
+"{\n"
+"    selection-background-color: #D1DBCB;\n"
+"    background-color: #31363B;\n"
+"    border-style: solid;\n"
+"    border: 1px solid #76797C;\n"
+"    border-radius: 2px;\n"
+"    padding: 5px;\n"
+"    min-width: 75px;\n"
+"}\n"
+"\n"
+"QPushButton:checked{\n"
+"    background-color: #76797C;\n"
+"    border-color: #6A6969;\n"
+"}\n"
+"\n"
+"QComboBox:hover,QPushButton:hover,QAbstractSpinBox:hover,QLineEdit:hover,QTextEdit:hover,QPlainTextEdit:hover,QAbstractView:hover,QTreeView:hover\n"
+"{\n"
+"    border: 1px solid #D1DBCB;\n"
+"}\n"
+"QTabWidget:focus, QCheckBox:focus, QRadioButton:focus, QSlider:focus\n"
+"{\n"
+"    border: none;\n"
+"}\n"
+"\n"
+"QHeaderView::section\n"
+"{\n"
+"    background-color: #76797C;\n"
+"    color: #eff0f1;\n"
+"    padding: 5px;\n"
+"    border: 1px solid #76797C;\n"
+"}\n"
+"\n"
+"QSizeGrip {\n"
+"    image: url(:/qss_icons/rc/sizegrip.png);\n"
+"    width: 12px;\n"
+"    height: 12px;\n"
+"}\n"
+"\n"
+"\n"
+"QMainWindow::separator\n"
+"{\n"
+"    background-color: #323232;\n"
+"    color: white;\n"
+"    padding-left: 4px;\n"
+"    spacing: 2px;\n"
+"    border: 1px dashed #76797C;\n"
+"}\n"
+"\n"
+"QMainWindow::separator:hover\n"
+"{\n"
+"\n"
+"    background-color: #787876;\n"
+"    color: white;\n"
+"    padding-left: 4px;\n"
+"    border: 1px solid #76797C;\n"
+"    spacing: 2px;\n"
+"}\n"
+"\n"
+"\n"
+"QMenu::separator\n"
+"{\n"
+"    height: 1px;\n"
+"    background-color: #76797C;\n"
+"    color: white;\n"
+"    padding-left: 4px;\n"
+"    margin-left: 10px;\n"
+"    margin-right: 5px;\n"
+"}\n"
+"\n"
+"QFrame\n"
+"{\n"
+"    border-radius: 0px;\n"
+"    /*border: 1px solid #76797C;*/\n"
+"}\n"
+"\n"
+"\n"
+"QFrame[frameShape=\"0\"]\n"
+"{\n"
+"    border-radius: 0px;\n"
+"    border: 0px transparent #76797C;\n"
+"}\n"
+"\n"
+"QStackedWidget\n"
+"{\n"
+"    border: 1px transparent black;\n"
+"}"))
         self.b1 = QtGui.QPushButton(Dialog)
         self.b1.setGeometry(QtCore.QRect(10, 230, 61, 51))
         self.b1.setObjectName(_fromUtf8("b1"))
@@ -56,20 +239,17 @@ class Ui_Dialog(object):
         self.decimal.setGeometry(QtCore.QRect(150, 290, 61, 51))
         self.decimal.setObjectName(_fromUtf8("decimal"))
         self.display = QtGui.QLineEdit(Dialog)
-        self.display.setGeometry(QtCore.QRect(10, 20, 341, 71))
+        self.display.setGeometry(QtCore.QRect(10, 20, 371, 71))
         self.display.setObjectName(_fromUtf8("display"))
         self.equal = QtGui.QPushButton(Dialog)
         self.equal.setGeometry(QtCore.QRect(220, 290, 61, 51))
         self.equal.setObjectName(_fromUtf8("equal"))
-
         self.clear = QtGui.QPushButton(Dialog)
-        self.clear.setGeometry(QtCore.QRect(220, 110, 61, 51))
+        self.clear.setGeometry(QtCore.QRect(290, 170, 91, 51))
         self.clear.setObjectName(_fromUtf8("clear"))
-
-	self.back = QtGui.QPushButton(Dialog)
-        self.back.setGeometry(QtCore.QRect(290, 110, 61, 51))
+        self.back = QtGui.QPushButton(Dialog)
+        self.back.setGeometry(QtCore.QRect(290, 230, 91, 51))
         self.back.setObjectName(_fromUtf8("back"))
-
         self.add = QtGui.QPushButton(Dialog)
         self.add.setGeometry(QtCore.QRect(220, 170, 61, 51))
         self.add.setObjectName(_fromUtf8("add"))
@@ -77,28 +257,28 @@ class Ui_Dialog(object):
         self.substract.setGeometry(QtCore.QRect(220, 230, 61, 51))
         self.substract.setObjectName(_fromUtf8("substract"))
         self.divide = QtGui.QPushButton(Dialog)
-        self.divide.setGeometry(QtCore.QRect(290, 230, 61, 51))
+        self.divide.setGeometry(QtCore.QRect(290, 290, 91, 51))
         self.divide.setObjectName(_fromUtf8("divide"))
         self.multiply = QtGui.QPushButton(Dialog)
-        self.multiply.setGeometry(QtCore.QRect(290, 170, 61, 51))
+        self.multiply.setGeometry(QtCore.QRect(220, 110, 61, 51))
         self.multiply.setObjectName(_fromUtf8("multiply"))
         self.sq_root = QtGui.QPushButton(Dialog)
-        self.sq_root.setGeometry(QtCore.QRect(290, 290, 61, 51))
+        self.sq_root.setGeometry(QtCore.QRect(320, 350, 61, 51))
         self.sq_root.setObjectName(_fromUtf8("sq_root"))
         self.sin = QtGui.QPushButton(Dialog)
-        self.sin.setGeometry(QtCore.QRect(10, 350, 61, 51))
+        self.sin.setGeometry(QtCore.QRect(10, 350, 51, 51))
         self.sin.setObjectName(_fromUtf8("sin"))
         self.cos = QtGui.QPushButton(Dialog)
-        self.cos.setGeometry(QtCore.QRect(80, 350, 61, 51))
+        self.cos.setGeometry(QtCore.QRect(70, 350, 51, 51))
         self.cos.setObjectName(_fromUtf8("cos"))
         self.tan = QtGui.QPushButton(Dialog)
-        self.tan.setGeometry(QtCore.QRect(150, 350, 61, 51))
+        self.tan.setGeometry(QtCore.QRect(130, 350, 51, 51))
         self.tan.setObjectName(_fromUtf8("tan"))
         self.power = QtGui.QPushButton(Dialog)
-        self.power.setGeometry(QtCore.QRect(220, 350, 61, 51))
+        self.power.setGeometry(QtCore.QRect(150, 410, 61, 51))
         self.power.setObjectName(_fromUtf8("power"))
         self.log = QtGui.QPushButton(Dialog)
-        self.log.setGeometry(QtCore.QRect(290, 350, 61, 51))
+        self.log.setGeometry(QtCore.QRect(250, 350, 61, 51))
         self.log.setObjectName(_fromUtf8("log"))
         self.b_open = QtGui.QPushButton(Dialog)
         self.b_open.setGeometry(QtCore.QRect(10, 410, 61, 51))
@@ -107,14 +287,57 @@ class Ui_Dialog(object):
         self.b_close.setGeometry(QtCore.QRect(80, 410, 61, 51))
         self.b_close.setObjectName(_fromUtf8("b_close"))
         self.ln = QtGui.QPushButton(Dialog)
-        self.ln.setGeometry(QtCore.QRect(150, 410, 61, 51))
+        self.ln.setGeometry(QtCore.QRect(190, 350, 51, 51))
         self.ln.setObjectName(_fromUtf8("ln"))
         self.e = QtGui.QPushButton(Dialog)
         self.e.setGeometry(QtCore.QRect(220, 410, 61, 51))
         self.e.setObjectName(_fromUtf8("e"))
         self.pi = QtGui.QPushButton(Dialog)
-        self.pi.setGeometry(QtCore.QRect(290, 410, 61, 51))
+        self.pi.setGeometry(QtCore.QRect(290, 410, 91, 51))
         self.pi.setObjectName(_fromUtf8("pi"))
+        self.bco = QtGui.QPushButton(Dialog)
+        self.bco.setGeometry(QtCore.QRect(200, 350, 181, 51))
+        self.bco.setObjectName(_fromUtf8("bco"))
+        self.arg = QtGui.QPushButton(Dialog)
+        self.arg.setGeometry(QtCore.QRect(10, 350, 181, 51))
+        self.arg.setObjectName(_fromUtf8("arg"))
+        self.r1 = QtGui.QRadioButton(Dialog)
+        self.r1.setGeometry(QtCore.QRect(290, 110, 91, 51))
+        self.r1.setObjectName(_fromUtf8("r1"))
+        self.arg.raise_()
+        self.b1.raise_()
+        self.b3.raise_()
+        self.b2.raise_()
+        self.b6.raise_()
+        self.b5.raise_()
+        self.b4.raise_()
+        self.b9.raise_()
+        self.b8.raise_()
+        self.b7.raise_()
+        self.plus_minus.raise_()
+        self.b0.raise_()
+        self.decimal.raise_()
+        self.display.raise_()
+        self.equal.raise_()
+        self.clear.raise_()
+        self.back.raise_()
+        self.add.raise_()
+        self.substract.raise_()
+        self.divide.raise_()
+        self.multiply.raise_()
+        self.sq_root.raise_()
+        self.sin.raise_()
+        self.cos.raise_()
+        self.tan.raise_()
+        self.power.raise_()
+        self.log.raise_()
+        self.b_open.raise_()
+        self.b_close.raise_()
+        self.ln.raise_()
+        self.e.raise_()
+        self.pi.raise_()
+        self.r1.raise_()
+        self.bco.raise_()
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -135,7 +358,7 @@ class Ui_Dialog(object):
         self.decimal.setText(_translate("Dialog", ".", None))
         self.equal.setText(_translate("Dialog", "=", None))
         self.clear.setText(_translate("Dialog", "AC", None))
-	self.back.setText(_translate("Dialog", "Back", None))
+        self.back.setText(_translate("Dialog", "Back", None))
         self.add.setText(_translate("Dialog", "+", None))
         self.substract.setText(_translate("Dialog", "-", None))
         self.divide.setText(_translate("Dialog", "/", None))
@@ -150,4 +373,8 @@ class Ui_Dialog(object):
         self.b_close.setText(_translate("Dialog", ")", None))
         self.ln.setText(_translate("Dialog", "ln", None))
         self.e.setText(_translate("Dialog", "e", None))
-        self.pi.setText(_translate("Dialog", "pi", None))
+        self.pi.setText(_translate("Dialog", "π", None))
+        self.bco.setText(_translate("Dialog", "j", None))
+        self.arg.setText(_translate("Dialog", "Arg", None))
+        self.r1.setText(_translate("Dialog", "Complex", None))
+
--- logic.py
+++ logic.py
@@ -3,12 +3,38 @@ import sys
 from PyQt4 import QtGui
 import calculator
 import math
+import cmath
 prev= ""
 class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
+	def btnstate(self,r1):
+		if r1.isChecked() == True:
+			self.bco.show()
+			self.arg.show()
+			self.sin.hide()
+			self.cos.hide()
+			self.tan.hide()
+			self.log.hide()
+			self.ln.hide()
+			self.sq_root.hide()
+
+		else:
+			self.bco.hide()
+			self.arg.hide()
+			self.sin.show()
+			self.cos.show()
+			self.tan.show()
+			self.log.show()
+			self.ln.show()
+			self.sq_root.show()
+			self.e.show()
 
 	def __init__(self):
 		super(calculator_class, self).__init__()
 		self.setupUi(self)
+		self.r1.setChecked(False)
+		self.bco.hide()
+		self.arg.hide()
+		self.r1.toggled.connect(lambda:self.btnstate(self.r1))
 		self.b0.clicked.connect(lambda:self.display_screen('0'))
 		self.b0.clicked.connect(lambda:self.storage('0',1))
 		self.b1.clicked.connect(lambda:self.display_screen('1'))
@@ -29,8 +55,10 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 		self.b8.clicked.connect(lambda:self.storage('8',1))
 		self.b9.clicked.connect(lambda:self.display_screen('9'))
 		self.b9.clicked.connect(lambda:self.storage('9',1))
+		self.bco.clicked.connect(lambda:self.display_screen('j'))
+		self.bco.clicked.connect(lambda:self.storage('j',1))
 		self.decimal.clicked.connect(lambda:self.display_screen('.'))
-		self.decimal.clicked.connect(lambda:self.storage('.',1))		
+		self.decimal.clicked.connect(lambda:self.storage('.',1))
 		self.add.clicked.connect(lambda:self.display_screen(' + '))
 		self.add.clicked.connect(lambda:self.storage(' + ',1))
 		self.substract.clicked.connect(lambda:self.display_screen(' - '))
@@ -49,6 +77,8 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 		self.pi.clicked.connect(lambda:self.storage('math.pi',1))
 		self.e.clicked.connect(lambda:self.display_screen('e'))
 		self.e.clicked.connect(lambda:self.storage('math.e',1))
+		self.arg.clicked.connect(lambda:self.display_screen('Arg'))
+		self.arg.clicked.connect(lambda:self.storage('(180/(math.pi))*cmath.phase',1))
 		self.b_open.clicked.connect(lambda:self.display_screen(' ( '))
 		self.b_open.clicked.connect(lambda:self.storage(' ( ',1))
 		self.sin.clicked.connect(lambda:self.display_screen(' sin( '))
@@ -69,13 +99,15 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 		self.back.clicked.connect(lambda:self.display_screen2(self.prev_disp))
 		self.back.clicked.connect(lambda:self.storage1(self.store))
 		self.equal.clicked.connect(self.calculation)
-		self.display.setReadOnly(True)	
-	
+		self.display.setReadOnly(True)
+
 	store= ""
 	prev_disp = ""
 	stack = []
-	stack_disp = []	
+	stack_disp = []
 	temp = []
+
+
 	def storage1(self,value):
 		try:
 			self.stack.pop()
@@ -96,7 +128,7 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 	def display_screen(self,value):
 		self.display.insert(value)
 		self.stack_disp.append(value)
-		
+
 	def display_screen1(self,value):
 		self.display.setText(value)
 		self.stack_disp.append(value)
@@ -108,7 +140,7 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 			self.display_screen1("")
 		else:
 			self.temp = self.stack_disp
-			value = ''.join(self.temp)		
+			value = ''.join(self.temp)
 			self.display.setText(value)
 
 	def display_error(self,value):
@@ -123,7 +155,7 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 		screen_value=self.store
 		screen_value=str(screen_value)
 		print(''.join(self.stack))
-		try:		
+		try:
 			final_value=eval(screen_value)
 		except ZeroDivisionError:
 			print("Math Error : Division by Zero")
@@ -141,14 +173,14 @@ class calculator_class(calculator.Ui_Dialog,QtGui.QMainWindow):
 			print("Input Error : Please enter proper input")
 			self.stack.append('#')
 			self.display_error("Input Error : Please enter proper input")
-		else:		
+		else:
 			final_value=str(final_value)
 			self.store=final_value
 			self.stack.append(final_value)
-			print(self.store)	
+			print(self.store)
 			self.disp_res(" = " + final_value)
-	
-	
+
+
 
 if __name__== '__main__':
 	qapp=QtGui.QApplication(sys.argv)
